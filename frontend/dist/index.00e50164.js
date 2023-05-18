4c29f8569844737ed77f2a080d9ebea7f9ed3bd0
timsntech <tim_stehle@posteo.de>
2023-05-18 22:36:50 +0200

    remove cache files

--- frontend/dist/index.00e50164.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-let haushaltsbuch = new Haushaltsbuch();
-haushaltsbuch.start();
-
-//# sourceMappingURL=index.00e50164.js.map
--- frontend/dist/index.aa69868b.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-let haushaltsbuch = new Haushaltsbuch();
-haushaltsbuch.start();
-
-//# sourceMappingURL=index.aa69868b.js.map
