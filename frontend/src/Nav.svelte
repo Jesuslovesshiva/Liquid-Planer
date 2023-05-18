f67b6f1d8e24c92398d4760e2e73546f75fa91ef
timsntech <tim_stehle@posteo.de>
2023-05-18 21:23:10 +0200

    init svelte app

---
+++ frontend/src/Nav.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let markenname = "Liqui-Planner";
+</script>
+  
+<nav id="navigationsleiste">
+    <a href="#">
+      <span id="markenname">{markenname}</span>
+    </a>
+</nav>
+  
+<style>
+</style>
+  
